d81980acefd11ef6e9b346a1bf438e9a2cc4f82f
Noel Mangwarira <ncmangwarira@gmail.com>
2025-07-21 22:30:41 +0200

    -- save changes

--- interfaces/index.ts
+++ interfaces/index.ts
@@ -14,3 +14,25 @@ export interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElemen
   isLoading?: boolean;
   children: React.ReactNode;
 }
+export interface Address {
+    state: string;
+    city: string;
+    country: string;
+}
+
+export interface Offers {
+    bed: string;
+    shower: string;
+    occupants: string;
+}
+
+export interface PropertyProps {
+    name: string;
+    address: Address;
+    rating: number;
+    category: string[];
+    price: number;
+    offers: Offers;
+    image: string;
+    discount: string;
+}
